6fa1296cd2bcaa8930156a1a86515e7a653855e5
CAI Pengfei <caipengfei_ecpkn@outlook.com>
2019-12-09 10:42:52 +0800

    initial commit

---
+++ src/main/java/com/moonrabbit/web/toyblog/ToyblogApplication.java
@@ -0,0 +1,13 @@
+package com.moonrabbit.web.toyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moonrabbit/web/toyblog/ToyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonrabbit.web.toyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
